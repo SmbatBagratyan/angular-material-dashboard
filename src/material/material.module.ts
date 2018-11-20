f9a9c1f18e2dbedbf262731124093fc888f5ade5
Smbat <smb.bagratyan@gmail.com>
2018-11-20 18:25:00 +0400

    angular-material-init

---
+++ src/material/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+@NgModule({
+    imports: [MatButtonModule, MatCheckboxModule,FlexLayoutModule],
+    exports: [MatButtonModule, MatCheckboxModule,FlexLayoutModule],
+})
+export class MaterialModule { }
